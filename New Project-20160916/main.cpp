e9b811bff6e844b8d01a6a868930350cab300dcc
cdub9 <coleman.warren@hotmail.com>
2016-09-15 18:15:02 -0600

    Created by Coding Ground Client

---
+++ New Project-20160916/main.cpp
@@ -0,0 +1,55 @@
+/*
+Coleman Warren, Section 8, coleman.warren@hotmail.com
+
+Purpose:
+Input:
+Output:
+Test Cases
+Test Case 1
+Input:
+Output:
+Test Case 2
+Input:
+Output:
+Test Case 3
+Input:
+Output:
+*/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+//Constants
+double PI = 3.14159;
+double LARGE_PIZZA_PRICE = 14.68;
+double MEDIUM_PIZZA_PRICE = 11.48;
+double SMALL_PIZZA_PRICE = 7.28;
+double LARGE_PIZZA_DIAMETER = 20;
+double MEDIUM_PIZZA_DIAMETER = 16;
+double SMALL_PIZZA_DIAMETER = 12;
+int GUESTS_PER_LARGE_PIZZA = 7;
+int GUESTS_PER_MEDIUM_PIZZA = 3;
+int GUESTS_PER_SMALL_PIZZA = 1;
+
+int main() {
+	//Determine the number of attending guests
+
+	cout << "How many guests will be attending the event? " << endl;
+	int number_of_guests;
+	cin >> number_of_guests;
+
+	//Compute the number of pizzas to order
+
+	int large_pizzas_to_order = number_of_guests / GUESTS_PER_LARGE_PIZZA;
+    int medium_pizzas_to_order = (number_of_guests % GUESTS_PER_LARGE_PIZZA) / GUESTS_PER_MEDIUM_PIZZA;
+    int small_pizzas_to_order = number_of_guests - (large_pizzas_to_order * GUESTS_PER_LARGE_PIZZA + medium_pizzas_to_order * GUESTS_PER_MEDIUM_PIZZA);
+    
+    cout << large_pizzas_to_order << endl;
+    cout << medium_pizzas_to_order;
+
+
+	return 0;
+}
